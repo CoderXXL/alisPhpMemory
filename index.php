eae8001421bfac000c1d54bee249098705d3b47c
Dario Lang <dario.lang@flagbit.de>
2021-08-23 13:40:40 +0200

    chore: New folder structure

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Alis Cooles Memory-Spiel</title>
 </head>
 <body>
