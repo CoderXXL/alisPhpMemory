2dec0201eb79f34b3b59ab33f8f33cc8fb190990
Dario Lang <dario.lang@flagbit.de>
2021-08-23 13:19:42 +0200

    chore: First Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Alis Cooles Memory-Spiel</title>
+</head>
+<body>
+<h1>Memory</h1>
+
+</body>
+</html>
