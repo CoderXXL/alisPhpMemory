8282228936934e2f6567c382b38e1f12889bd0a2
Dario Lang <dario.lang@flagbit.de>
2021-09-02 16:13:55 +0200

    feat: Add memory functions

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,41 +2,49 @@ let data = [
     {
         "name": "Card1",
         "img": "https://png.pngtree.com/png-clipart/20200225/original/pngtree-strawberry-vector-illustration-isolated-on-white-background-strawberry-clip-art-png-image_5261937.jpg",
+        "color": "red",
         "id": 1
     },
     {
         "name": "Card2",
         "img": "https://png.pngtree.com/png-clipart/20200225/original/pngtree-strawberry-vector-illustration-isolated-on-white-background-strawberry-clip-art-png-image_5261937.jpg",
+        "color": "blue",
         "id": 2
     },
     {
         "name": "Card3",
         "img": "https://png.pngtree.com/png-clipart/20200225/original/pngtree-strawberry-vector-illustration-isolated-on-white-background-strawberry-clip-art-png-image_5261937.jpg",
+        "color": "yellow",
         "id": 3
     },
     {
         "name": "Card4",
         "img": "https://png.pngtree.com/png-clipart/20200225/original/pngtree-strawberry-vector-illustration-isolated-on-white-background-strawberry-clip-art-png-image_5261937.jpg",
+        "color": "green",
         "id": 4
     },
     {
         "name": "Card5",
         "img": "https://png.pngtree.com/png-clipart/20200225/original/pngtree-strawberry-vector-illustration-isolated-on-white-background-strawberry-clip-art-png-image_5261937.jpg",
+        "color": "aqua",
         "id": 5
     },
     {
         "name": "Card6",
         "img": "https://png.pngtree.com/png-clipart/20200225/original/pngtree-strawberry-vector-illustration-isolated-on-white-background-strawberry-clip-art-png-image_5261937.jpg",
+        "color": "pink",
         "id": 6
     },
     {
         "name": "Card7",
         "img": "https://png.pngtree.com/png-clipart/20200225/original/pngtree-strawberry-vector-illustration-isolated-on-white-background-strawberry-clip-art-png-image_5261937.jpg",
+        "color": "limegreen",
         "id": 7
     },
     {
         "name": "Card8",
         "img": "https://png.pngtree.com/png-clipart/20200225/original/pngtree-strawberry-vector-illustration-isolated-on-white-background-strawberry-clip-art-png-image_5261937.jpg",
+        "color": "skyblue",
         "id": 8
     }
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,87 @@
 class Memory {
-    init(cards) {
-        this.shuffleCards(cards);
-        this.setup(cards);
-        this.setEventListener(cards);
-    }
+  init(cards) {
+    shuffleCards(cards);
+    this.setup(cards);
+    this.setEventListener(cards);
+  }
 
-    setup(cards) {
-        let html = this.getHtml();
-        this.buildCardsHtml(cards, html.playground);
-    }
+  setup(cards) {
+    let html = this.getMemoryHtml();
+    this.buildCardsHtml(cards, html.playground);
+  }
 
-    getHtml() {
-        const playground = document.getElementById('playground');
-        const cards = document.getElementsByClassName('card');
+  getMemoryHtml() {
+    const playground = document.getElementById("playground");
+    const cards = document.getElementsByClassName("card");
 
-        return {
-            "playground": playground,
-            "cards": cards
-        };
-    }
+    return {
+      playground: playground,
+      cards: cards,
+    };
+  }
 
-    buildCardsHtml(cards, playground) {
-        let cardHtml = '';
-        for(let card of cards) {
-            cardHtml += `
-            <div class="card" data-id="${card.id}">
-                <div class="inner">
-                    <div class="front">
-                        <img src="${card.img}" alt="${card.name}">
-                    </div>
-                    <div class="back">back</div>
+  buildCardsHtml(cards, playground) {
+    let cardHtml = "";
+    for (let card of cards) {
+      cardHtml += `
+            <div class="card">
+                <div class="inner" data-id="${card.id}">
+                    <div class="front" style="background-color: ${card.color}"></div>
+                    <div class="back"></div>
                 </div>
             </div>
             `;
-        }
-        playground.innerHTML = cardHtml;
     }
+    playground.innerHTML = cardHtml;
+  }
 
-    setEventListener() {
-        const playground = this.getHtml()["playground"];
-        playground.addEventListener('click', (e) => {
-            console.log(e);
-            e.target.classList.toggle("flipped");
-        });
-    }
+  setEventListener() {
+    const playground = this.getMemoryHtml()["playground"];
+    let counter = 0;
+    let solved = 0;
+    let locked, cardsEqual = false;
+    let tempArr = [];
 
-    shuffleCards(arr) {
-        for (let i = arr.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [arr[i], arr[j]] = [arr[j], arr[i]];
+    playground.addEventListener("click", (e) => {
+      if (!locked) {
+        if (
+          !e.target.className.includes("flipped") &&
+          e.target.className.includes("inner")
+        ) {
+          counter++;
+          e.target.classList.add("flipped");
+          tempArr.unshift(e.target);
+
+          if (counter % 2 === 0) {
+            if (tempArr[0].dataset.id === tempArr[1].dataset.id) {
+              solved++;
+              cardsEqual = true;
+            } else {
+              cardsEqual = false;
+            }
+
+            locked = true;
+            setTimeout(() => {
+              if (!cardsEqual) {
+                tempArr[0].classList.remove("flipped");
+                tempArr[1].classList.remove("flipped");
+              }
+              locked = false;
+            }, 1000);
+          }
+
+          if(solved === data.length) {
+            alert('Gut gemacht');
+          }
         }
-        return arr;
-    }
+      }
+    });
+  }
 }
 
 window.onload = () => {
-    const cards = data.concat(data);
+  const cards = data.concat(data);
 
-    let memory = new Memory();
-    memory.init(cards);
-}
+  let memory = new Memory();
+  memory.init(cards);
+};
