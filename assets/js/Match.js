a97456f5537f87432ead4fe3d57b96d34645763a
SebastianGrigarczik <sebsatian.grigarczik@flagbit.de>
2021-10-07 13:54:41 +0200

    fix: rewrite colorpicker

--- assets/js/Match.js
+++ assets/js/Match.js
@@ -51,113 +51,128 @@ function addClickEvent(card) {
 }
 
 function cardColors() {
-    let max = game.getNumberCouples();
-    let min = 2;
+    let max = 30;
+    let count = 0;
 
-    let colors = new Array2D(30, 2);
+    let cardOne;
+    let cardTwo;
+
+    let colors = new Array2D(30, 1);
 
     colors.items[0][0] = "#00FFFF"; //cyan
-    colors.items[0][1] = 0;
+    colors.items[0][1] = false;
 
     colors.items[1][0] = "#00BFFF"; //DeepSkyBlue
-    colors.items[1][1] = 0;
+    colors.items[1][1] = false;
 
     colors.items[2][0] = "#7FFFD4"; //Aquamarine
-    colors.items[2][1] = 0;
+    colors.items[2][1] = false;
 
     colors.items[3][0] = "#0000FF"; //Blue
-    colors.items[3][1] = 0;
+    colors.items[3][1] = false;
     
     colors.items[4][0] = "#006400"; //DarkGreen
-    colors.items[4][1] = 0;
+    colors.items[4][1] = false;
 
     colors.items[5][0] = "#8FBC8F"; //DarkSeaGreen
-    colors.items[5][1] = 0;
+    colors.items[5][1] = false;
 
     colors.items[6][0] = "#90EE90"; //LightGreen
-    colors.items[6][1] = 0;
+    colors.items[6][1] = false;
 
     colors.items[7][0] = "#00FF00"; //Lime
-    colors.items[7][1] = 0;
+    colors.items[7][1] = false;
 
     colors.items[8][0] = "#FF8C00"; //DarkOrange
-    colors.items[8][1] = 0;
+    colors.items[8][1] = false;
 
     colors.items[9][0] = "#8A2BE2"; //BlueViolet
-    colors.items[9][1] = 0;
+    colors.items[9][1] = false;
 
     colors.items[10][0] = "#8B008B"; //DarkMagenta
-    colors.items[10][1] = 0;
+    colors.items[10][1] = false;
 
     colors.items[11][0] = "#EE82EE"; //Violet
-    colors.items[11][1] = 0;
+    colors.items[11][1] = false;
 
     colors.items[12][0] = "#A9A9A9"; //DarkGray
-    colors.items[12][1] = 0;
+    colors.items[12][1] = false;
 
     colors.items[13][0] = "#2F4F4F"; //DarkSlateGray
-    colors.items[13][1] = 0;
+    colors.items[13][1] = false;
 
     colors.items[14][0] = "#696969"; //DimGray
-    colors.items[14][1] = 0;
+    colors.items[14][1] = false;
 
     colors.items[15][0] = "#778899"; //LightSlateGray
-    colors.items[15][1] = 0;
+    colors.items[15][1] = false;
 
     colors.items[16][0] = "#FF4500"; //OrangeRed
-    colors.items[16][1] = 0;
+    colors.items[16][1] = false;
 
     colors.items[17][0] = "#FF0000"; //Red
-    colors.items[17][1] = 0;
+    colors.items[17][1] = false;
 
     colors.items[18][0] = "#CD5C5C"; //IndianRed
-    colors.items[18][1] = 0;
+    colors.items[18][1] = false;
 
     colors.items[19][0] = "#DC143C"; //Crimson
-    colors.items[19][1] = 0;
+    colors.items[19][1] = false;
 
     colors.items[20][0] = "#A52A2A"; //Brown
-    colors.items[20][1] = 0;
+    colors.items[20][1] = false;
 
     colors.items[21][0] = "#8B4513"; //SaddleBrown
-    colors.items[21][1] = 0;
+    colors.items[21][1] = false;
 
     colors.items[22][0] = "#F4A460"; //SandyBrown
-    colors.items[22][1] = 0;
+    colors.items[22][1] = false;
 
     colors.items[23][0] = "#FFFF00"; //Yellow
-    colors.items[23][1] = 0;
+    colors.items[23][1] = false;
 
     colors.items[24][0] = "#FFFFE0"; //LightYellow
-    colors.items[24][1] = 0;
+    colors.items[24][1] = false;
 
     colors.items[25][0] = "#B8860B"; //DarkGoldenRod
-    colors.items[25][1] = 0;
+    colors.items[25][1] = false;
 
     colors.items[26][0] = "#FF1493"; //DeepPink
-    colors.items[26][1] = 0;
+    colors.items[26][1] = false;
 
     colors.items[27][0] = "#FFD700"; //Gold
-    colors.items[27][1] = 0;
+    colors.items[27][1] = false;
 
     colors.items[28][0] = "#FF69B4"; //HotPink
-    colors.items[28][1] = 0;
+    colors.items[28][1] = false;
 
     colors.items[29][0] = "#4682B4"; //SteelBlue
-    colors.items[29][1] = 0;
+    colors.items[29][1] = false;
 
-    for (let id = 0; id < (game.getNumberCouples() * 2); id++) {
-        game.getCard(id).setColor(generateColor());
+    while (count < game.getNumberCouples()) {
+        let idOne = Math.floor(Math.random() * (game.getNumberCouples() * 2));
+        let idTwo = Math.floor(Math.random() * (game.getNumberCouples() * 2));
+
+        cardOne = game.getCard(idOne);
+        cardTwo = game.getCard(idTwo);
+
+        if (idOne != idTwo) {
+            if (cardOne.getColor() == "black" && cardTwo.getColor() == "black") {
+                generateColor();
+                count++;
+            }
+        }
     }
 
     function generateColor() {
-        let random = Math.floor(Math.random() * max);
+        let randomColor = Math.floor(Math.random() * max);
 
-        if (colors.items[random][1] >= min) {
+        if (colors.items[randomColor][1]) {
             return generateColor();
         } else {
-            colors.items[random][1] = colors.items[random][1] + 1;
-            return colors.items[random][0];
+            colors.items[randomColor][1] = true;
+            cardOne.setColor(colors.items[randomColor][0]);
+            cardTwo.setColor(colors.items[randomColor][0]);
         }
     }
 }
